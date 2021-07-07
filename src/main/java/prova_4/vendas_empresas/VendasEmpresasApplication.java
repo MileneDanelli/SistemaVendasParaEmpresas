39cd8fd724d9f4587b3812682a31a204cb905972
MileneDanelli <mi.danelli@outlook.com>
2021-07-07 19:21:49 -0300

    primeiro commit

---
+++ src/main/java/prova_4/vendas_empresas/VendasEmpresasApplication.java
@@ -0,0 +1,13 @@
+package prova_4.vendas_empresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasEmpresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasEmpresasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prova_4/vendas_empresas/VendasEmpresasApplicationTests.java
@@ -0,0 +1,13 @@
+package prova_4.vendas_empresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasEmpresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
